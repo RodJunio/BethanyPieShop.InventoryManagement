fdeea2030db9efbf8c696f344cae14b425434a3d
Junio Rodrigues <devjuniorodrigues@gmail.com>
2023-12-16 14:38:42 -0300

    Add project files.

---
+++ BethanyPieShop.InventoryManagement/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace BethanyPieShop.InventoryManagement;
+
+public class Product
+{
+    private int id;
+    private string name = string.Empty;
+    private string? description;
+
+    private int maxItemsInStock = 0;
+
+    private UnitType? unitType;
+    private int amoyntInStock = 0;
+    private bool isBelowStockThreshold = false;
+}
---
+++ BethanyPieShop.InventoryManagement/UnitType.cs
@@ -0,0 +1,8 @@
+﻿namespace BethanyPieShop.InventoryManagement;
+
+public enum UnitType
+{
+    PerItem,
+    PerBox,
+    PerKg
+}
